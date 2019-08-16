947d0d0f19fcd982a4e567522ea15d87bd85f34e
Tim Pote <pote.timothy@gmail.com>
2019-08-15 19:31:19 -0500

    Handle some input

--- src/nashville/lisp_sync/webapp.clj
+++ src/nashville/lisp_sync/webapp.clj
@@ -1,20 +1,26 @@
 (ns nashville.lisp-sync.webapp
   (:require
+    [clojure.tools.logging :as log]
     [hiccup.core :as hiccup]
-    [ring.adapter.jetty :as jetty]))
+    [ring.adapter.jetty :as jetty]
+    [ring.middleware.params :as params]))
 
-(defn app [req]
+(defn app [{m :request-method
+            {input "mah-button"} :form-params}]
   {:status 200
-   :body (hiccup/html
-           [:body
-            [:div
-             [:form
-              [:input {:name "mah-button"}]
-              [:button {:type :submit}]]]])})
+   :body (if (= m :post)
+           (str "Hello, " input "!")
+           (hiccup/html
+             [:body
+              [:div
+               [:form {:action "/"
+                       :method :post}
+                [:input {:name "mah-button"}]
+                [:button {:type :submit}]]]]))})
 
 (defn start-server []
   (jetty/run-jetty (fn [req]
-                     (app req))
+                     (app (params/params-request req)))
                    {:port 8080
                     :join? false}))
 
