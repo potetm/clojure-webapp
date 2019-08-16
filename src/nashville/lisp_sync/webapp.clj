24d9943a32b06878a304a6d926923090e972624a
Tim Pote <pote.timothy@gmail.com>
2019-08-15 19:10:54 -0500

    Added hiccup

--- src/nashville/lisp_sync/webapp.clj
+++ src/nashville/lisp_sync/webapp.clj
@@ -1,1 +1,22 @@
-(ns nashville.lisp-sync.webapp)
+(ns nashville.lisp-sync.webapp
+  (:require
+    [hiccup.core :as hiccup]
+    [ring.adapter.jetty :as jetty]))
+
+(defn app [req]
+  {:status 200
+   :body (hiccup/html
+           [:body
+            [:div
+             [:span "Hello, Lisp!"]]])})
+
+(defn start-server []
+  (jetty/run-jetty (fn [req]
+                     (app req))
+                   {:port 8080
+                    :join? false}))
+
+(comment
+  (def s (start-server))
+
+  (.stop s))
